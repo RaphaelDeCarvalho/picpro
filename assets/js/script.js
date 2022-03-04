104108a9122a41fb07e75b4438eb686cedbbceb9
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2022-03-04 19:03:16 -0300

    alterado estilo, script e html para resolução de bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ function eventoClick(event){
     let objeto = event.target;
     const botao = objeto.id;
             const opcao = document.querySelector(`.secaoTransacao .container div[data-id="${botao}"]`);
+            const opcaoBox1 = document.querySelector(".secaoTransacao__boxOpcao_1");
+            const opcaoBox2 = document.querySelector(".secaoTransacao__boxOpcao_2");
+            const opcaoBox3 = document.querySelector(".secaoTransacao__boxOpcao_3");
             removeClasse();
             opcao.classList.add("mostrar");
+            opcaoBox1.classList.add("mostrar");
+            opcaoBox2.classList.add("mostrar");
+            opcaoBox3.classList.add("mostrar");
 }
 function removeClasse(){
     const secao = document.querySelectorAll(".secaoTransacao .container div");
